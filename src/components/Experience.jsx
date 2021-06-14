2b7b2c9403d2c2fa1683090fe6db2649eabd9582
nahuelcaceres <nahuelcaceres@users.noreply.github.com>
2021-06-13 22:26:43 -0300

    Agregando exportacion a pdf

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -9,15 +9,24 @@ const Experience = props => (
         <H2Styled name="Experiencias"></H2Styled>
         <div className="Experience-container">
 
-            {props.data.map((experience, index) => (
+            {props.data.map((experience, index) => {
+                return (
+                    <div className="Experience item" key={`Experiences-${index}`}>
+                        <H3Styled>{experience.jobTitle} @ {experience.company}
+                            <span>{experience.startDate} - {experience.endDate}</span>
+                        </H3Styled>
 
-                <div className="Experience item" key={`Experiences-${index}`}>
-                    <H3Styled>{experience.jobTitle} @ {experience.company}
-                        <span>{experience.startDate} - {experience.endDate}</span>
-                    </H3Styled>
-                    <PStyled name={experience.jobDescription}></PStyled>
-                </div>
-            ))}
+                        <PStyled name={experience.jobDescription}></PStyled>
+
+                        {experience.descriptions.map((description, idx) => {
+                            return (
+                                <PStyled key={`descr-${idx}`} name={description.description} />
+                            )
+                        })}
+                        <br />
+                    </div>
+                )
+            })}
 
         </div>
     </div>
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
-import { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import styled from 'styled-components';
 import Main from '../components/Main';
 import Sidebar from '../components/Sidebar';
 import Info from '../components/Info';
 import About from '../components/About';
-import Education from '../components/Education';
+// import Education from '../components/Education';
 import Experience from '../components/Experience';
 import Certificates from '../components/Certificates';
 import Skills from '../components/Skills';
 import useGetData from '../hooks/useGetData'
+import jsPDF from 'jspdf'
 
 const GlobalStyle = createGlobalStyle`
     body {
@@ -19,18 +21,51 @@ const GlobalStyle = createGlobalStyle`
     }
 `;
 
+const ButtonGeneratePDF = styled.button`
+    color: #212121;
+    border: none;
+    text-decoration: none;
+    font-size: 2.2em;
+`;
+
 const App = () => {
     //Traemos la informacion con el hook
     const data = useGetData(); //Ejecutando primero
     //console.log(data);
 
+    const generatePDF = () => {
+
+        const pdfDoc = new jsPDF("l", "pt", [1080, 1080]);
+        pdfDoc.lineHeightProportion = 2;
+        // doc.viewerPreferences({ 'FitWindow': true }, true);
+
+        document.querySelector("#btExportPDF").remove();
+        document.querySelector(".About-social").remove();
+
+        const html = document.querySelector("#app > div > div");
+        
+        pdfDoc.html(html, {
+            callback: function (pdf) {
+                pdf.save("Nahuel_Caceres.pdf");
+
+                window.location.reload();
+            },
+            margin: [25,25,25,25]
+        })
+
+    }
+
     //if data.length no tiene nada, mostrar cargando..caso contrario
     //si hacer el return de toda nuestra estructura
     return data.length === 0 ? <h1>Cargando...</h1> : (
-        <Main>
+        <Main id="main">
             <GlobalStyle />
             <Sidebar>
-                <About 
+                <ThemeProvider theme={{ color: "#0e76a8" }}>
+                    <ButtonGeneratePDF id="btExportPDF" className="fa fa-file-pdf-o" onClick={() => generatePDF()} />
+                </ThemeProvider>
+
+                <About
                     avatar={data.avatar}
                     name={data.name}
                     profession={data.profession}
@@ -39,18 +74,18 @@ const App = () => {
                     social={data.social}
                 />
             </Sidebar>
-            
+
             <Info>
-                <Experience 
+                <Experience
                     data={data.experience}
                 />
                 {/* <Education 
                     data={data.education}
                 /> */}
-                <Certificates 
+                <Certificates
                     data={data.certificate}
                 />
-                <Skills 
+                <Skills
                     data={data.skills}
                 />
             </Info>
